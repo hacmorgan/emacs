75c0b43937eb4f2fddaf2a24c3b744c9f043e065
hacmorgan <ham430@gmail.com>
2020-06-04 18:34:33 +1000

    start of emacs repo

---
+++ elpa/exec-path-from-shell-20191229.112/exec-path-from-shell-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "exec-path-from-shell" "20191229.112" "Get environment variables such as $PATH from the shell" 'nil :commit "9365c94ef18a2d58e73d47e0fe878c7d5007a827" :keywords '("unix" "environment") :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "https://github.com/purcell/exec-path-from-shell")
