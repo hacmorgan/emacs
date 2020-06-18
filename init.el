7cd92cd334cfc13b32fa411e6fc4755d42cc2c27
hacmorgan <ham430@gmail.com>
2020-06-18 21:34:27 +1000

    customize-face

--- init.el
+++ init.el
@@ -56,14 +56,16 @@
     (powerline cyberpunk-theme org evil csv-mode magit haskell-mode ein markdown-mode company)))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil)
- '(vc-follow-symlinks t))
+ '(vc-follow-symlinks t)
+ '(xterm-mouse-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#d3d3d3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "UKWN" :family "Fantasque Sans Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "#000000" :distant-foreground "orange red" :foreground "#d3d3d3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight semi-light :height 105 :width expanded :foundry "PfEd" :family "Iosevka"))))
  '(cursor ((t (:background "violet"))))
+ '(font-lock-type-face ((t (:foreground "pale green"))))
  '(line-number-current-line ((t (:inherit line-number :foreground "magenta"))))
  '(mode-line ((t (:background "#333333" :foreground "DeepSkyBlue1" :box nil))))
  '(mode-line-inactive ((t (:background "#1A1A1A" :foreground "#4D4D4D" :box nil))))
