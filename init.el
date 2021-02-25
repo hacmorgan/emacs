daed4f49c4a9646206baac493ab3adfdba47d068
Hamish Morgan <ham430@gmail.com>
2021-02-25 16:47:27 +1100

    fix merge conflict

--- init.el
+++ init.el
@@ -46,7 +46,7 @@
      ("s" . "src")
      ("v" . "verse")))
  '(package-selected-packages
-   '(fira-code-mode clang-format cmake-ide cmake-mode systemd rainbow-mode arduino-mode rust-mode lsp-ui flycheck lsp-mode tidal powerline cyberpunk-theme org evil csv-mode magit haskell-mode ein markdown-mode company))
+   '(fira-code-mode yaml-mode clang-format cmake-ide cmake-mode systemd rainbow-mode arduino-mode rust-mode lsp-ui flycheck lsp-mode tidal powerline cyberpunk-theme org evil csv-mode magit haskell-mode ein markdown-mode company))
  '(pixel-scroll-mode nil)
  '(powerline-default-separator 'utf-8)
  '(powerline-height 20)
