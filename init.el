e4b318a1e8d6c6acc3736d59b44255665910c2bd
Hamish Morgan <ham430@gmail.com>
2021-08-14 23:33:16 +1000

    Eink theme is a thing, whatever that is

--- init.el
+++ init.el
@@ -51,7 +51,7 @@
      ("s" . "src")
      ("v" . "verse")))
  '(package-selected-packages
-   '(lsp-jedi company-jedi company-irony irony fira-code-mode yaml-mode clang-format cmake-ide cmake-mode systemd rainbow-mode arduino-mode rust-mode lsp-ui flycheck lsp-mode tidal powerline cyberpunk-theme org evil csv-mode magit haskell-mode ein markdown-mode company))
+   '(eink-theme lsp-jedi company-jedi company-irony irony fira-code-mode yaml-mode clang-format cmake-ide cmake-mode systemd rainbow-mode arduino-mode rust-mode lsp-ui flycheck lsp-mode tidal powerline cyberpunk-theme org evil csv-mode magit haskell-mode ein markdown-mode company))
  '(pixel-scroll-mode nil)
  '(powerline-default-separator 'utf-8)
  '(powerline-height 20)
