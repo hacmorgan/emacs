7d83db14052dc56ee78edc1b3311c98b5dc7fc92
Hamish Morgan <ham430@gmail.com>
2022-05-20 03:55:55 +0000

    Undo black --preview, does not work

--- init.el
+++ init.el
@@ -7,7 +7,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(blacken-executable "black --preview")
  '(blink-cursor-mode nil)
  '(blink-matching-paren nil)
  '(c-basic-offset 4)
